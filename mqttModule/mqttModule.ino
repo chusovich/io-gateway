54c7f6e47663b34f714594474c6b4b3d3f151d2b
Caleb H <147654545+chusovich@users.noreply.github.com>
2024-02-29 09:59:04 -0500

    update file names

--- mqttModule/mqttModule.ino
+++ mqttModule/mqttModule.ino
@@ -8,10 +8,11 @@
 #include "secret.h"
 
 // ----------- freeRTOS objects ----------- //
-Task mqttTask("MQTT Task", 2048, 0);
+Task mqttConnectionTask("MQTT Connection Manager", 1024, 0);
+Task mqttPubSubTask("MQTT ")
 Task serialInTask("Serial Read Task", 1024, 1);
 Task serialOutTask("Serial Write Task", 1024, 1);
-Task displayTask("Display Update Task", 2048, 1);
+// Task displayTask("Display Update Task", 2048, 1);
 
 Queue incomingMsgs(25);
 Queue outgoingMsgs(25);
@@ -28,7 +29,7 @@ void setup() {
   mqttTask.createTask(TaskMQTT,2);
   serialInTask.createTask(TaskSerialRead,1);
   serialOutTask.createTask(TaskSerialWrite,1);
-  displayTask.createTask(TaskDisplay,1);
+  // displayTask.createTask(TaskDisplay,1);
 
   incomingMsgs.create();
   outgoingMsgs.create();
